8fca53f04bcb2c86076d6c4b3efd8dcf7c5e758e
David McKay <david@rawkode.com>
2020-10-26 10:35:02 +0000

    feat: allow building from source for opencv upgrade

--- shell.nix
+++ shell.nix
@@ -1,4 +1,22 @@
-with (import <nixpkgs> { });
+{ pkgs ? import <nixpkgs> {
+  overlays =  [
+    (self: super:
+{
+  opencv4 = super.opencv4.overrideAttrs (old: {
+    version = "4.5.0";
+
+    src = super.fetchFromGitHub {
+      owner = "opencv";
+      repo = "opencv";
+      rev    = "4.5.0";
+      sha256 = "0kv64hinfb07m7rld8b5iy8qkwi465rl1v8pf3fvd2ankmyb1m5w";
+    };
+  });
+}) ];
+}}:
+
+with pkgs;
+
 mkShell {
   buildInputs = [
     go
